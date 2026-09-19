faea7db3e8155e3e7316b21d0da6bc75f6435e46
rupa-9777 <rupasreeputta9999@gmail.com>
2026-09-19 16:31:53 +0530

    Add Activity 1

---
+++ Activity01/Account.java
@@ -0,0 +1,37 @@
+public class Account {
+    private String accountNumber;
+    private String holderName;
+    private double balance;
+
+    public Account(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+}
